a0f4dec69bf1bd7869fd2a9464ea85a1478991a5
Bastien <bastien.desmarquoy@gmail.com>
2015-02-19 16:06:15 +0100

    Resolution de la première étape du problème des List Le projet compile

--- grails-app/domain/projetggts/QuestionnaireDetaille.groovy
+++ grails-app/domain/projetggts/QuestionnaireDetaille.groovy
@@ -1,7 +1,7 @@
 package projetggts
 
 class QuestionnaireDetaille {
-//	List<HashMap<String, String>> questions;
+	ArrayList<HashMap<String, String>> questions;
 	String delai; //Le passage int->String et String->int sera fait dans le controlleur.
 	static belongsTo = [questionnaire: QuestionnaireCours];
 	static hasMany = [reponses: ReponseDetaille];
--- grails-app/domain/projetggts/ReponseDetaille.groovy
+++ grails-app/domain/projetggts/ReponseDetaille.groovy
@@ -1,7 +1,7 @@
 package projetggts
 
 class ReponseDetaille {
-//	List<HashMap<String, String>> reponses;
+	ArrayList<HashMap<String, String>> reponses;
 	static belongsTo = [questionnaireDetaille: QuestionnaireDetaille];
 
     static constraints = {
