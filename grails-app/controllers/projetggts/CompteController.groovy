f6e694b00b5ed1f0767cde325bc04ea518ffcee7
mcaillet <caillet.m74@gmail.com>
2015-02-23 10:50:46 +0100

    Base pour l'authentification (attention rajout de fichiers GSP)

--- grails-app/controllers/projetggts/CompteController.groovy
+++ grails-app/controllers/projetggts/CompteController.groovy
@@ -2,5 +2,33 @@ package projetggts
 
 class CompteController {
 
-    def scaffold = true;
+    def index = {}
+	
+	def login = {}
+	
+	def authentification = {
+	  def compte = Compte.findByIdentifiantAndMdp(params.identifiant, params.mdp)
+	  if(compte){
+		session.compte = compte
+		flash.message = "Bonjour ${compte.nom}!"
+		if(compte.admin){
+			redirect(controller:"compte.admin", action:"index")
+		}
+		if(compte.eleve){
+			redirect(controller:"compte.eleve", action:"index")
+		}
+		if(compte.professeur){
+			redirect(controller:"compte.professeur", action:"index")
+		}
+	  }else{
+		flash.message = "${params.identifiant} ou mot de passe invalide!."
+		redirect(action:"login")
+	  }
+	}
+	
+	def logout = {
+	  flash.message = "Aurevoir ${session.compte.nom}"
+	  session.compte = null
+	  redirect(controller:"compte", action:"authenticate")
+	}
 }
---
+++ grails-app/views/compte/index.gsp
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <meta name="layout" content="main" />
+    <title>Index</title>         
+  </head>
+  <body>
+  	<g:link controller="compte" action="login">Se connecter</g:link>
+  </body>
+</html>
+ 
