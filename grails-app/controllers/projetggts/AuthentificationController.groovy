898f44c7129ee67a027d36d4531b9ce08a46c07b
mcaillet <caillet.m74@gmail.com>
2015-02-25 11:39:36 +0100

    Mise à jour du Controller d'Authentification

--- grails-app/controllers/projetggts/AuthentificationController.groovy
+++ grails-app/controllers/projetggts/AuthentificationController.groovy
@@ -8,13 +8,13 @@ class AuthentificationController {
 	def login = {}
 	
 	def authentification = {
-	  def compte = Compte.findByIdentifiantAndMdp(params.identifiant, params.mdp)
+	  def compte = Compte.findByIdentifiantAndMdp(params.login, params.password)
 	  if(compte){
 		session.compte = compte
 		flash.message = "Bonjour ${compte.nom}!"
-		redirect(controller:"compte",action:"show")//=> Par défaut! Il faut prendre en compte le fait que la personne qui se connecte est soit un prof soit un élève soit un administrateur (modifier les classes du domaines selon le nouveau diagramme de classe UML
+		redirect(controller:"compte",action:"index")//=> Par défaut! Il faut prendre en compte le fait que la personne qui se connecte est soit un prof soit un élève soit un administrateur (modifier les classes du domaines selon le nouveau diagramme de classe UML
 	  }else{
-		flash.message = "${params.identifiant} ou mot de passe invalide!."
+		flash.message = "${params.login} ou mot de passe invalide!."
 		redirect(action:"login")
 	  }
 	}
